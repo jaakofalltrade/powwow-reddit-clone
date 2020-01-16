994e66607275a570f071f9f57c77d3be59282504
jaakofalltrade <jaako.andes@boom.camp>
2020-01-16 11:35:43 +0800

    Fixed the model error, and added more models

--- backend/app/powwow_api/models.py
+++ backend/app/powwow_api/models.py
@@ -4,7 +4,6 @@ from django.db import models
 from django.utils import timezone
 
 class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
 
@@ -16,10 +15,9 @@ class User(models.Model):
 
 
 class Community(models.Model):
-    community_id = models.AutoField(primary_key=True)
-    community_name = models.CharField(max_length=50, default=None, null=True, blank=True)
-    community_description = models.CharField(max_length=150, default=None, blank=True, null=True)
-    date_created = models.DateTimeField('date created', default=None)
+    community_name = models.CharField(max_length=50)
+    community_description = models.CharField(max_length=150)
+    date_created = models.DateTimeField('date created')
     def __str__(self):
         return f'{self.community_name}'
 
@@ -27,10 +25,9 @@ class Community(models.Model):
         verbose_name_plural = "community"
 
 class CommunityMember(models.Model):
-    cm_id = models.AutoField(primary_key=True)
     community_id = models.ForeignKey(Community, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_joined = models.DateTimeField('date published')
+    date_joined = models.DateTimeField('date joined')
 
     def __str__(self):
         return f'{self.id}'
